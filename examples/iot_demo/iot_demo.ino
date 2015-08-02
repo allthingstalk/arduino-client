21686546f0654a948d5dd641aec7f75512eb3c78
Jan Bogaerts <jb@allthingstalk.com>
2015-08-02 19:56:41 +0200

    improves demo with min and max values for sensor

--- examples/iot_demo/iot_demo.ino
+++ examples/iot_demo/iot_demo.ino
@@ -60,7 +60,7 @@ void setup()
   
   if(Device.Connect(&ethClient, httpServer))                //connect the device with the IOT platform.
   {
-    Device.AddAsset(knobPin, "knob", "rotary switch",false, "integer");
+    Device.AddAsset(knobPin, "knob", "rotary switch",false, "{'type': 'integer', 'minimum': 0, 'maximum': 1023");
     Device.AddAsset(ledPin, "led", "light emitting diode", true, "boolean");
     Device.Subscribe(pubSub);                               // make certain that we can receive message from the iot platform (activate mqtt)
   }
