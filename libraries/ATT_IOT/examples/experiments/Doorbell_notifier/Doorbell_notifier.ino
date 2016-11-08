14396e38f01761cc29645ea87ba04cf57626d80e
Jan Bogaerts <jb@allthingstalk.com>
2016-11-08 16:46:30 +0100

    update licensw with date instead of project name

--- libraries/ATT_IOT/examples/experiments/Doorbell_notifier/Doorbell_notifier.ino
+++ libraries/ATT_IOT/examples/experiments/Doorbell_notifier/Doorbell_notifier.ino
@@ -1,5 +1,5 @@
 /*
-   Copyright arduino-client AllThingsTalk
+   Copyright 2014-2016 AllThingsTalk
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
